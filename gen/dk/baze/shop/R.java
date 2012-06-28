bb5eb2ecc06b3bd78896af1eba62fe9aff1ee229
Zach Ewert <zachanima@gmail.com>
2012-06-28 15:50:13 +0200

    Add layout prototype

--- gen/dk/baze/shop/R.java
+++ gen/dk/baze/shop/R.java
@@ -9,7 +9,7 @@ package dk.baze.shop;
 
 public final class R {
     public static final class array {
-        public static final int quantities_array=0x7f050000;
+        public static final int sites_array=0x7f050000;
     }
     public static final class attr {
     }
@@ -17,8 +17,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int Spinner01=0x7f060007;
+        public static final int Spinner02=0x7f060009;
+        public static final int TextView01=0x7f060006;
+        public static final int TextView02=0x7f060008;
         public static final int imageView1=0x7f060000;
-        public static final int quantity=0x7f060001;
+        public static final int spinner1=0x7f060005;
+        public static final int textView1=0x7f060001;
+        public static final int textView2=0x7f060002;
+        public static final int textView3=0x7f060003;
+        public static final int textView4=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
