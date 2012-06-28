2f2d9b11d130a4b5abefba41ecf6ff08ebf0a5e6
Zach Ewert <zachanima@gmail.com>
2012-06-28 13:10:28 +0200

    Reference spinner entries in main.xml

--- src/dk/baze/shop/ShopActivity.java
+++ src/dk/baze/shop/ShopActivity.java
@@ -11,11 +11,5 @@ public class ShopActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    Spinner spinner = (Spinner)findViewById(R.id.quantity);
-    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-      this, R.array.quantities_array, android.R.layout.simple_spinner_item
-    );
-    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    spinner.setAdapter(adapter);
   }
 }
