e2a015efc500672450b0fe5ae5361e01886ec2cf
Zach Ewert <zachanima@gmail.com>
2012-06-28 13:08:12 +0200

    Add spinner

--- gen/dk/baze/shop/R.java
+++ gen/dk/baze/shop/R.java
@@ -8,10 +8,17 @@
 package dk.baze.shop;
 
 public final class R {
+    public static final class array {
+        public static final int quantities_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static final int imageView1=0x7f060000;
+        public static final int quantity=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
